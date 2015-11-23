a6c8334a95ceb2c973ebd62f39ec998c72d63355
Andrew Garner <andrew@andrewgarner.com>
2015-11-23 15:20:18 +0000

    Replace Airbrake with Bugsnag

--- config/initializers/airbrake.rb
+++
@@ -1,8 +0,0 @@
-# This file is overwritten on deploy
-#
-Airbrake.configure do |config|
-  # Adding production to the development environments causes Airbrake not
-  # to attempt to send notifications.
-  config.development_environments << "production"
-  config.params_filters += ["current_password", "password-strength-score"]
-end

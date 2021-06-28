dfe0ddb0ec4ec5bf55299fd9bc6e9dd5c87a7552
Ben Lovell <benjamin.lovell@gmail.com>
2021-06-28 10:13:20 +0100

    Switch to MoneyHelper domain

--- test/integration/passphrase_reset_test.rb
+++ test/integration/passphrase_reset_test.rb
@@ -45,7 +45,7 @@ class PassphraseResetTest < ActionDispatch::IntegrationTest
 
       open_email(user.email)
       assert current_email
-      assert_match /noreply-signon-development@.*\.gov\.uk/, current_email.from[0]
+      assert_match /noreply-pensionwise-signon-development@.*\.org\.uk/, current_email.from[0]
       assert_equal nil, last_email.reply_to[0]
       assert_equal "Reset passphrase instructions", current_email.subject
 

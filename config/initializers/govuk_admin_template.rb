9a5df4b1360ec91b1131eb461a1437d1ceaa6979
Ben Lovell <benjamin.lovell@gmail.com>
2022-08-09 11:02:04 +0100

    Add environment labelling
    This should help users know which environment they're performing actions
    in.

--- config/initializers/govuk_admin_template.rb
+++ config/initializers/govuk_admin_template.rb
@@ -1,3 +1,5 @@
 # We were setting app_title here, but we needed to use the department.name from
 # localisations, which aren't available in initializers. Our solution was to move
 # it to app/views/application.html.erb.
+GovukAdminTemplate.environment_style = Rails.env.staging? ? 'preview' : ENV['RAILS_ENV']
+GovukAdminTemplate.environment_label = Rails.env.titleize

b3a19564aa767db59960b57cbf0b2c77a5b7d72c
emilysatodossantos <emily.satodossantos@solirius.com>
2023-11-24 14:51:52 +0000

    Remove duplicate methods

--- lib/sso_push_credential.rb
+++ lib/sso_push_credential.rb
@@ -5,7 +5,9 @@ class SsoPushCredential
   class UserNotProvided < StandardError; end
 
   class << self
-    attr_accessor :user_email, :user
+    attr_accessor :user_email
+
+    attr_writer :user
 
     def credentials(application)
       user.grant_application_permissions(application, PERMISSIONS)

81565270c3ac13baa04fb610de6be727547d90ed
emilysatodossantos <emily.satodossantos@solirius.com>
2023-11-27 17:19:16 +0000

    Prefer `each_value` and pass `&:to_sym` as an argument instead of block

--- db/migrate/20150121092250_migrate_application_and_permissions_to_batch_invitation_application_permissions_join_table.rb
+++ db/migrate/20150121092250_migrate_application_and_permissions_to_batch_invitation_application_permissions_join_table.rb
@@ -4,7 +4,7 @@ class MigrateApplicationAndPermissionsToBatchInvitationApplicationPermissionsJoi
 
     BatchInvitation.all.each do |batch_invitation|
       _supported_permissions = []
-      batch_invitation.applications_and_permissions.values.each do |permission_attributes|
+      batch_invitation.applications_and_permissions.each_value do |permission_attributes|
         application_id, permissions = permission_attributes['application_id'], permission_attributes['permissions']
         if permissions.present?
           _supported_permissions << SupportedPermission.where(application_id: application_id,

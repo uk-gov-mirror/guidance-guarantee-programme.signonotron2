67177e7023834eb2c66100425dc63161ccfc2485
emilysatodossantos <emily.satodossantos@solirius.com>
2023-11-28 11:54:18 +0000

    Remove parallel assigment

--- db/migrate/20150121092250_migrate_application_and_permissions_to_batch_invitation_application_permissions_join_table.rb
+++ db/migrate/20150121092250_migrate_application_and_permissions_to_batch_invitation_application_permissions_join_table.rb
@@ -5,7 +5,8 @@ class MigrateApplicationAndPermissionsToBatchInvitationApplicationPermissionsJoi
     BatchInvitation.all.each do |batch_invitation|
       _supported_permissions = []
       batch_invitation.applications_and_permissions.each_value do |permission_attributes|
-        application_id, permissions = permission_attributes['application_id'], permission_attributes['permissions']
+        application_id = permission_attributes['application_id']
+        permissions = permission_attributes['permissions']
         if permissions.present?
           _supported_permissions << SupportedPermission.where(application_id: application_id,
                                                               name: permissions)

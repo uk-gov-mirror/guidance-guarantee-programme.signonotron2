b478ac349c315bc2d8e61197f35c11da2e91a67a
Ben Lovell <benjamin.lovell@gmail.com>
2026-03-29 14:30:57 +0100

    Remove the deprecations before 7.2

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Signonotron2
 
   class Application < Rails::Application
     config.load_defaults 6.0
+
+    config.active_support.cache_format_version = 7.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
